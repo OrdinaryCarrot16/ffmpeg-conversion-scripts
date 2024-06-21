8c5d5d015303c3d4340401f0430c1840e09f3bcf
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2024-06-20 19:20:03 -0700

    Update Finding Nemo.ps1

--- Videos/Blu-Ray/Movies/Finding Nemo/Finding Nemo.ps1
+++ Videos/Blu-Ray/Movies/Finding Nemo/Finding Nemo.ps1
@@ -17,7 +17,7 @@ Rename-Item -Path "Finding Nemo_t20.mkv" -NewName "Finding Nemo.mkv"
 if ($rip_encode -eq "2") {
 New-Item -Path "Finding Nemo (Converted)" -ItemType Directory
 
-ffmpeg -i "Finding Nemo.avs" -c:v libvpx-vp9 -cpu-used 2 -row-mt 1 -crf 20 -b:v 10M -an "Finding Nemo_TMP.webm"
+ffmpeg -i "Finding Nemo.avs" -c:v libvpx-vp9 -cpu-used 2 -row-mt 1 -crf 20 -b:v 8M -an "Finding Nemo_TMP.webm"
 ffmpeg -i "Finding Nemo.avs" -c:a libopus -b:a 192k "Finding Nemo_TMP.opus"
 mkvmerge -o "Finding Nemo.webm" --webm ("Finding Nemo_TMP.webm") ("Finding Nemo_TMP.opus") --display-dimensions 0:1280x720 --default-duration 0:23.976fps
 Move-Item -Path "Finding Nemo.webm" -Destination "Finding Nemo (Converted)/Finding Nemo.webm"
