bd380e8cbbef436a425709754dc7753a176efe7a
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2024-07-21 15:53:12 -0700

    Update Cloudy With A Chance Of Meatballs.ps1

--- Videos/Blu-Ray/Movies/Cloudy With A Chance Of Meatballs/Cloudy With A Chance Of Meatballs.ps1
+++ Videos/Blu-Ray/Movies/Cloudy With A Chance Of Meatballs/Cloudy With A Chance Of Meatballs.ps1
@@ -17,6 +17,7 @@ Rename-Item -Path "Cloudy With A Chance Of Meatballs_t10.mkv" -NewName "Cloudy W
 if ($rip_encode -eq "2") {
 New-Item -Path "Cloudy With A Chance Of Meatballs (Converted)" -ItemType Directory
 
+# Estimated File Size: 1.2 GB
 ffmpeg -i "Cloudy With A Chance Of Meatballs.avs" -c:v libvpx-vp9 -cpu-used 2 -row-mt 1 -crf 20 -b:v 10M -an "Cloudy With A Chance Of Meatballs_TMP.webm"
 ffmpeg -i "Cloudy With A Chance Of Meatballs.avs" -c:a libopus -b:a 192k "Cloudy With A Chance Of Meatballs_TMP.opus"
 mkvmerge -o "Cloudy With A Chance Of Meatballs.webm" --webm ("Cloudy With A Chance Of Meatballs_TMP.webm") ("Cloudy With A Chance Of Meatballs_TMP.opus") --display-dimensions 0:1280x720 --default-duration 0:23.976fps
